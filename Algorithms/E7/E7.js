75c088795c325479346ef6a31906cf701c5426bf
Mario Laureano <cmlaurreano@intekglobal.com>
2022-12-30 19:51:15 -0600

    Exercise 7 is not completed yet

--- Algorithms/E7/E7.js
+++ Algorithms/E7/E7.js
@@ -2,20 +2,6 @@ function longestRunOfTwoNumbers(numberString) {
     if (numberString.length < 3) {
         return numberString;
     }
-    // let strLength = numberString.length;
-    // let runStart = 0;
-    // let longestRun = numberString[0];
-
-    // for (let i = 1; i < strLength; i++) {
-    //     let substring = numberString.slice(runStart, i + 1)
-    //     let numbers = new Set(substring);
-    //     if (numbers.size < 3 && substring.length > longestRun.length) {
-    //         longestRun = substring;
-    //     } else if (numbers.size > 2) {
-    //         runStart++;
-    //     }
-    // }
-    // return longestRun;
     let output = numberString[0];
     let start = 0;
     let isolatedNumbers = new Set(numberString[0]);
