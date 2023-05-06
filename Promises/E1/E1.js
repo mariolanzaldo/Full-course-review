3a46d4e6f8f0e0cac9cb71ed1801cafa2e51342a
Mario Laureano <cmlaurreano@intekglobal.com>
2023-05-05 19:28:59 -0600

    E1 clock test missing

--- Promises/E1/E1.js
+++ Promises/E1/E1.js
@@ -1,23 +1,58 @@
 const runBatches = async (tasks, pool_size) => {
-
     if (pool_size === 0) throw new Error("No task ran. Select a positive pool size");
 
-    const batch = Math.min(pool_size, tasks.length);
+    let activeTasks = [];
     const results = [];
 
-    const tasksQueue = Array.from({ length: batch }, async () => {
-        for (const [index, task] of tasks.entries()) {
-            try {
-                results[index] = `{value: ${await task()}}`;
-            } catch (error) {
-                results[index] = `{error: ${error}}`;
-            }
+    for (const [index, task] of tasks.entries()) {
+        let activeTask;
+        if (activeTasks.length >= pool_size) {
+            await Promise.race(activeTasks);
         }
-    });
 
-    await Promise.all(tasksQueue);
+        try {
+            activeTask = await task();
+            activeTasks.splice(activeTasks.indexOf(activeTask), 1);
+            results[index] = `{value: ${activeTask}}`;
+
+
+        } catch (error) {
+            activeTasks.splice(activeTasks.indexOf(activeTask), 1);
+            results[index] = `{error: ${error}}`;
+        }
+
+        activeTasks.push(activeTask)
+
+    }
 
     return results;
-};
+}
+
+
+// (async function () {
+//     const taskFactorySample = (delay, resolve, val) => {
+//         return () => {
+//             return new Promise((res, rej) =>
+//                 setTimeout((value) => {
+//                     // fn();
+//                     console.log('called');
+//                     return resolve ? res(value) : rej(value)
+//                 }, delay, val))
+//         }
+//     };
+
+//     const pool_size = 2;
+
+//     const tasks = [
+//         taskFactorySample(500, true, 1),
+//         taskFactorySample(1000, true, 2),
+//         taskFactorySample(5000, false, 'error'),
+//         taskFactorySample(2000, true, 4),
+//         taskFactorySample(1000, false, 'error'),
+//         taskFactorySample(1000, false, 'error')
+//     ];
+//     const result = await runBatches(tasks, pool_size);
+//     console.log(result)
+// })()
 
 module.exports = runBatches;
--- Promises/E1/E1.test.js
+++ Promises/E1/E1.test.js
@@ -1,11 +1,24 @@
 const runBatches = require("./E1");
-// const batchTasks = require('./E1');
-jest.setTimeout(11000);
+// const FakeTimers = require("@sinonjs/fake-timers");
+
+// const fakeClock = FakeTimers.install();
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+
+jest.setTimeout(30000);
+// const fn = jest.fn();
 const taskFactorySample = (delay, resolve, val) => {
     return () => {
-        return new Promise((res, rej) => setTimeout(resolve ? res : rej, delay, val))
+        return new Promise((res, rej) =>
+            setTimeout((value) => {
+                // fn();
+                return resolve ? res(value) : rej(value)
+            }, delay, val))
     }
 };
+
 const tasks = [
     taskFactorySample(500, true, 1),
     taskFactorySample(1000, true, 2),
@@ -15,10 +28,31 @@ const tasks = [
     taskFactorySample(1000, false, 'error')
 ];
 
-test.skip('Test', async () => {
+test('Test', async () => {
     const pool_size = 2;
 
     const result = await runBatches(tasks, pool_size);
+
+    // await fakeClock.tickAsync(500);
+    // expect(fn).toHaveBeenCalledTimes(1);
+
+    // await fakeClock.tickAsync(500);
+    // expect(fn).toHaveBeenCalledTimes(2);
+
+    // await fakeClock.tickAsync(2000);
+    // expect(fn).toHaveBeenCalledTimes(3);
+
+    // await fakeClock.tickAsync(1000);
+    // expect(fn).toHaveBeenCalledTimes(4);
+
+    // await fakeClock.tickAsync(1000);
+    // expect(fn).toHaveBeenCalledTimes(5);
+
+    // await fakeClock.tickAsync(499);
+    // expect(fn).toHaveBeenCalledTimes(5);
+
+    // await fakeClock.tickAsync(1);
+    // expect(fn).toHaveBeenCalledTimes(6);
 
     return expect(result).toEqual([
         '{value: 1}',
@@ -33,6 +67,7 @@ test.skip('Test', async () => {
 test.skip('Pool larger than number of tasks', async () => {
     const pool_size = 200;
     const results = await runBatches(tasks, pool_size);
+    console.log(results);
 
     return expect(results).toEqual([
         '{value: 1}',
