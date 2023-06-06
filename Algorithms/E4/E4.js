fc09cad055a674943f5cd874d950e2274ad1fba5
Mario Laureano <cmlaurreano@intekglobal.com>
2023-06-06 17:05:39 -0600

    Some changes

--- Algorithms/E4/E4.js
+++ Algorithms/E4/E4.js
@@ -1,25 +1,3 @@
-// function mergeArrays(arr1, arrMergeIn) {
-//     let mergeIndex = 0;
-
-//     for (let i = 0; i < arr1.length; i++) {
-//         if (arr1[i] === undefined) {
-//             arr1[i] = arrMergeIn[mergeIndex++];
-//         }
-//         console.log(arr1)
-
-//         let j = i;
-
-//         while (j > 0 && arr1[j - 1] > arr1[j]) {
-//             [arr1[j], arr1[j - 1]] = [arr1[j - 1], arr1[j]]
-//             j--
-//         }
-
-//     }
-//     const output = arr1.filter((element) => element !== undefined);
-
-//     return output;
-// }
-
 function mergeArrays(largeArray, smallArray) {
 
     let largeArrayIndex = largeArray.length - 1;
